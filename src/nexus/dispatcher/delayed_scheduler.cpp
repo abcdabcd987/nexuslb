ed53d13cd38dc7b1401ce29865bea3c7fe53e6d2
Lequn Chen <chenlequn22@gmail.com>
2020-12-03 21:53:19 -0800

    delayed: make context structs value types

--- src/nexus/dispatcher/delayed_scheduler.cpp
+++ src/nexus/dispatcher/delayed_scheduler.cpp
@@ -3,12 +3,14 @@
 #include <glog/logging.h>
 
 #include <algorithm>
+#include <boost/asio/post.hpp>
 #include <chrono>
 #include <memory>
 #include <mutex>
 
 #include "nexus/common/model_def.h"
 #include "nexus/common/time_util.h"
+#include "nexus/common/typedef.h"
 
 namespace nexus {
 namespace dispatcher {
@@ -16,65 +18,58 @@ namespace delayed {
 
 namespace {
 
-bool OrderQueryContextByDeadlineASC(const std::unique_ptr<QueryContext>& lhs,
-                                    const std::unique_ptr<QueryContext>& rhs) {
-  return lhs->deadline() > rhs->deadline();
+bool OrderQueryContextByDeadlineASC(const std::shared_ptr<QueryContext>& lhs,
+                                    const std::shared_ptr<QueryContext>& rhs) {
+  return lhs->deadline > rhs->deadline;
 }
 
 }  // namespace
 
 QueryContext::QueryContext(QueryProto query_without_input, TimePoint deadline)
-    : query_without_input_(std::move(query_without_input)),
-      deadline_(deadline) {}
+    : proto(std::move(query_without_input)),
+      global_id(proto.global_id()),
+      deadline(deadline) {}
 
 InstanceContext::InstanceContext(ModelSession model_session, NodeId backend_id,
                                  const ModelProfile& profile)
-    : model_session_(std::move(model_session)),
-      backend_id_(backend_id),
-      profile_(profile) {
-  max_batch_ = profile_.GetMaxBatchWithFullBudget(model_session_.latency_sla());
+    : model_session(std::move(model_session)),
+      backend_id(backend_id),
+      profile(profile) {
+  max_batch =
+      profile.GetMaxBatchWithFullBudget(this->model_session.latency_sla());
 }
 
 ModelSessionContext::ModelSessionContext(ModelSession model_session)
-    : model_session_(std::move(model_session)) {
-  string_id_ = ModelSessionToString(model_session_);
-}
-
-std::shared_ptr<InstanceContext> ModelSessionContext::GetInstanceContext(
-    NodeId backend_id) const {
-  return instances_.at(backend_id);
-}
-
-void ModelSessionContext::AddInstanceContext(
-    NodeId backend_id, std::shared_ptr<InstanceContext> inst) {
-  instances_[backend_id] = inst;
-}
-
-void ModelSessionContext::EnqueueQuery(std::unique_ptr<QueryContext> qctx) {
-  pending_queries_.emplace_back(std::move(qctx));
-  std::push_heap(pending_queries_.begin(), pending_queries_.end(),
-                 OrderQueryContextByDeadlineASC);
+    : model_session(std::move(model_session)) {
+  string_id = ModelSessionToString(this->model_session);
 }
 
 BackendContext::BackendContext(NodeId backend_id)
-    : backend_id_(backend_id),
-      next_available_time_(std::chrono::nanoseconds(0)) {}
+    : backend_id(backend_id),
+      next_available_time(std::chrono::nanoseconds(0)) {}
+
+DelayedScheduler::DelayedScheduler()
+    : io_context_work_guard_(io_context_.get_executor()) {}
+
+void DelayedScheduler::RunAsWorker() { io_context_.run(); }
+
+void DelayedScheduler::Stop() { io_context_work_guard_.reset(); }
 
 void DelayedScheduler::AddModelSession(ModelSession model_session) {
   std::lock_guard<std::mutex> lock(mutex_);
-  auto mctx = std::make_unique<ModelSessionContext>(std::move(model_session));
-  if (models_.count(mctx->string_id())) {
+  auto mctx = std::make_shared<ModelSessionContext>(std::move(model_session));
+  if (models_.count(mctx->string_id)) {
     LOG(ERROR) << "Model session already exists. model_session="
-               << mctx->string_id();
+               << mctx->string_id;
     return;
   }
-  models_[mctx->string_id()] = std::move(mctx);
+  models_[mctx->string_id] = std::move(mctx);
 }
 
 void DelayedScheduler::AddBackend(NodeId backend_id) {
   std::lock_guard<std::mutex> lock(mutex_);
-  auto bctx = std::make_unique<BackendContext>(backend_id);
-  if (backends_.count(bctx->backend_id())) {
+  auto bctx = std::make_shared<BackendContext>(backend_id);
+  if (backends_.count(bctx->backend_id)) {
     LOG(ERROR) << "Backend already exists. backend_id=" << backend_id.t;
     return;
   }
@@ -88,12 +83,27 @@ void DelayedScheduler::EnqueueQuery(QueryProto query_without_input) {
       std::chrono::nanoseconds(query_without_input.clock().frontend_recv_ns()) +
       std::chrono::microseconds(model_session.latency_sla()));
   auto qctx =
-      std::make_unique<QueryContext>(std::move(query_without_input), deadline);
+      std::make_shared<QueryContext>(std::move(query_without_input), deadline);
 
-  std::lock_guard<std::mutex> lock(mutex_);
-  auto& mctx = models_.at(qctx->proto().model_session_id());
-  mctx->EnqueueQuery(std::move(qctx));
+  // Add to pending queries
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (queries_.count(qctx->global_id)) {
+      LOG(ERROR) << "Query already exists. global_id=" << qctx->global_id.t;
+      return;
+    }
+    queries_[qctx->global_id] = qctx;
+    auto& mctx = models_.at(qctx->proto.model_session_id());
+    mctx->sorted_queries.push_back(qctx);
+    std::push_heap(mctx->sorted_queries.begin(), mctx->sorted_queries.end(),
+                   OrderQueryContextByDeadlineASC);
+  }
+
+  // Trigger full schedule on the worker thread.
+  boost::asio::post(io_context_, [this] { WorkFullSchedule(); });
 }
+
+void DelayedScheduler::WorkFullSchedule() {}
 
 }  // namespace delayed
 }  // namespace dispatcher
--- src/nexus/dispatcher/delayed_scheduler.h
+++ src/nexus/dispatcher/delayed_scheduler.h
@@ -17,80 +17,62 @@ namespace nexus {
 namespace dispatcher {
 namespace delayed {
 
-class QueryContext {
- public:
+struct QueryContext {
   QueryContext(QueryProto query_without_input, TimePoint deadline);
 
-  QueryProto& proto() { return query_without_input_; }
-  TimePoint deadline() { return deadline_; }
-
- private:
-  QueryProto query_without_input_;
-  TimePoint deadline_;
+  QueryProto proto;
+  GlobalId global_id;
+  TimePoint deadline;
 };
 
-class InstanceContext {
- public:
+struct InstanceContext {
   InstanceContext(ModelSession model_session, NodeId backend_id,
                   const ModelProfile& profile);
 
-  const ModelSession& model_session() const { return model_session_; }
-  NodeId backend_id() const { return backend_id_; }
-  const ModelProfile& profile() const { return profile_; }
-  uint32_t max_batch() const { return max_batch_; }
-
- private:
-  ModelSession model_session_;
-  NodeId backend_id_;
-  const ModelProfile& profile_;
-  uint32_t max_batch_;
+  ModelSession model_session;
+  NodeId backend_id;
+  const ModelProfile& profile;
+  uint32_t max_batch;
 };
 
-class ModelSessionContext {
- public:
+struct ModelSessionContext {
   explicit ModelSessionContext(ModelSession model_session);
 
-  const ModelSession& model_session() const { return model_session_; }
-  const std::string& string_id() const { return string_id_; }
-
-  std::shared_ptr<InstanceContext> GetInstanceContext(NodeId backend_id) const;
-  void AddInstanceContext(NodeId backend_id,
-                          std::shared_ptr<InstanceContext> inst);
-  void EnqueueQuery(std::unique_ptr<QueryContext> qctx);
-
- private:
-  ModelSession model_session_;
-  std::string string_id_;
-  std::unordered_map<NodeId, std::shared_ptr<InstanceContext>> instances_;
-  std::vector<std::unique_ptr<QueryContext>> pending_queries_; // ORDER BY deadline ASC
+  ModelSession model_session;
+  std::string string_id;
+  std::unordered_map<NodeId, std::shared_ptr<InstanceContext>> instances;
+  // ORDER BY deadline ASC
+  std::vector<std::shared_ptr<QueryContext>> sorted_queries;
 };
 
-class BackendContext {
- public:
+struct BackendContext {
   explicit BackendContext(NodeId backend_id);
 
-  NodeId backend_id() const { return backend_id_; }
-
- private:
-  NodeId backend_id_;
-  std::unordered_map<std::string, std::shared_ptr<InstanceContext>> instances_;
-  TimePoint next_available_time_;
+  NodeId backend_id;
+  std::unordered_map<std::string, std::shared_ptr<InstanceContext>> instances;
+  TimePoint next_available_time;
 };
 
 class DelayedScheduler {
  public:
+  DelayedScheduler();
+  void RunAsWorker();
+  void Stop();
   void AddModelSession(ModelSession model_session);
   void AddBackend(NodeId backend_id);
   void EnqueueQuery(QueryProto query_without_input);
 
  private:
+  void WorkFullSchedule();
+
   boost::asio::io_context io_context_;
   boost::asio::executor_work_guard<boost::asio::io_context::executor_type>
       io_context_work_guard_;
 
   std::mutex mutex_;
-  std::unordered_map<std::string, std::unique_ptr<ModelSessionContext>> models_;
-  std::unordered_map<NodeId, std::unique_ptr<BackendContext>> backends_;
+  std::unordered_map<std::string, std::shared_ptr<ModelSessionContext>> models_;
+  std::unordered_map<NodeId, std::shared_ptr<BackendContext>> backends_;
+  std::unordered_map<GlobalId, std::shared_ptr<QueryContext>> queries_;
 };
 
 }  // namespace delayed

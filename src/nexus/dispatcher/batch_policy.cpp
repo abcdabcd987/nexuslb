4165f50e074c74d9d11001ab537f4bd80b0d331b
Lequn Chen <chenlequn22@gmail.com>
2021-03-16 10:04:05 -0700

    fix

--- src/nexus/dispatcher/batch_policy.cpp
+++ src/nexus/dispatcher/batch_policy.cpp
@@ -10,23 +10,22 @@ BatchResult BatchPolicyBySlidingWindowWithFreeLunch::Getbatch(
   BatchResult ret;
   auto proc_elapse = nanoseconds(0);
   // TODO: WithNStd
-  proc_elapse += nanoseconds(
-      static_cast<long>(profile.GetPreprocessLatency() * 1e3));
-  proc_elapse += nanoseconds(
-      static_cast<long>(profile.GetPostprocessLatency() * 1e3));
+  proc_elapse +=
+      nanoseconds(static_cast<long>(profile.GetPreprocessLatency() * 1e3));
+  proc_elapse +=
+      nanoseconds(static_cast<long>(profile.GetPostprocessLatency() * 1e3));
 
   // Sliding window policy
   std::vector<std::shared_ptr<QueryContext>> inputs, drops, remains;
   auto qiter = queries.begin();
   size_t qremain = queries.size();
-  uint32_t bs =
-      std::min(static_cast<uint32_t>(qremain), target_batch_size);
+  uint32_t bs = std::min(static_cast<uint32_t>(qremain), target_batch_size);
   while (inputs.size() < bs && qiter != queries.end()) {
     --qremain;
     const auto& qctx = *qiter;
     auto deadline = inputs.empty() ? qctx->deadline : inputs[0]->deadline;
-    auto fwd_elapse = nanoseconds(
-        static_cast<long>(profile.GetForwardLatency(bs) * 1e3));
+    auto fwd_elapse =
+        nanoseconds(static_cast<long>(profile.GetForwardLatency(bs) * 1e3));
     auto finish_time = exec_time + fwd_elapse + proc_elapse;
     if (deadline > finish_time) {
       inputs.push_back(qctx);
@@ -44,8 +43,8 @@ BatchResult BatchPolicyBySlidingWindowWithFreeLunch::Getbatch(
     const auto& qctx = *qiter;
     auto deadline = inputs.empty() ? qctx->deadline : inputs[0]->deadline;
     bs = inputs.size() + 1;
-    auto fwd_elapse = nanoseconds(
-        static_cast<long>(profile.GetForwardLatency(bs) * 1e3));
+    auto fwd_elapse =
+        nanoseconds(static_cast<long>(profile.GetForwardLatency(bs) * 1e3));
     auto finish_time = exec_time + fwd_elapse + proc_elapse;
     if (deadline > finish_time) {
       inputs.push_back(qctx);

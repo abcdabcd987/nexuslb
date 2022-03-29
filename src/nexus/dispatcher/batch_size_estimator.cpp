88a0ace83bc1c13e0f70d8ff6b80e04e95e18cd8
Lequn Chen <chenlequn22@gmail.com>
2022-03-29 00:54:08 +0000

    special treat n<1 when estimating target batchsize

--- src/nexus/dispatcher/batch_size_estimator.cpp
+++ src/nexus/dispatcher/batch_size_estimator.cpp
@@ -2,6 +2,30 @@
 
 namespace nexus {
 namespace dispatcher {
+namespace {
+
+struct Result {
+  uint32_t batch_size;
+  double num_gpus;
+};
+
+Result EstimateBatchSize(const ModelProfile& profile, double time_budget,
+                         double rps) {
+  // (0)  l(b) = alpha * b + beta
+  // (1)  n * (b / l(b)) == r
+  // (2)  l(b) * (1 + 1 / n) < SLO
+  for (uint32_t bs = 1;; ++bs) {
+    double l = profile.GetForwardLatency(bs) * 1e-6;
+    double t = bs / l;
+    double n = rps / t;
+    double n1 = n > 1 ? n : 1;
+    if (l * (1 + 1 / n1) > time_budget) {
+      return {std::max(1U, bs - 1), n};
+    }
+  }
+}
+
+}  // namespace
 
 BatchSizeEstimator::BatchSizeEstimator(double rps_multiplier,
                                        double std_multiplier)
@@ -10,16 +34,16 @@ BatchSizeEstimator::BatchSizeEstimator(double rps_multiplier,
 uint32_t BatchSizeEstimator::Estimate(const ModelProfile& profile,
                                       double time_budget, double rps,
                                       double std) const {
-  // (0)  l(b) = alpha * b + beta
-  // (1)  n * (b / l(b)) == xrps * r
-  // (2)  l(b) * (1 + 1 / n) < SLO
-  double r = rps + std * xstd_;
+  double r = rps * xrps_ + std * xstd_;
+  auto res = EstimateBatchSize(profile, time_budget, r);
+  if (res.num_gpus > 1) {
+    return res.batch_size;
+  }
+
+  // l(b) + b/r <= SLO, for n < 1, from Nexus paper, Line 13
   for (uint32_t bs = 2;; ++bs) {
     double l = profile.GetForwardLatency(bs) * 1e-6;
-    double t = bs / l;
-    double n = xrps_ * r / t;
-    double n1 = n > 1 ? n : 1;
-    if (l * (1 + 1 / n1) > time_budget) {
+    if (l + bs / r > time_budget) {
       return bs - 1;
     }
   }
